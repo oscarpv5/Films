a20df13aaa0d3e18313d7d40e769e28b86908894
opilladov <opilladov24@fpcoruna.afundacion.org>
2025-04-09 13:19:26 +0200

    Uploaded query params tests

--- Films/urls.py
+++ Films/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     path('print/<valor1>/JSON2/<valor2>', endpoint.prueba2),
     path('print/JSON3/<valor>', endpoint.prueba3),
     path('print/JSON4/<int:valor>', endpoint.prueba4),
+    path('print/JSON5', endpoint.prueba5),
+    path('print/JSON6/<int:valor>', endpoint.prueba6),
+    path('print/JSON7', endpoint.prueba7),
 ]
--- appFilms/endpoint.py
+++ appFilms/endpoint.py
@@ -15,3 +15,31 @@ def prueba3(request, valor):
 
 def prueba4(request, valor):
     return JsonResponse({"Estado": valor})
+
+def prueba5(request):
+    if request.method == "GET":
+        q = request.GET.get('q')
+        print(f"Query param q: {q}")
+        return JsonResponse({'query': q})
+
+    else:
+        return JsonResponse({"error": "HTTP method not supported"}, status=405)
+
+def prueba6(request, valor):
+    if request.method == "GET":
+        q = request.GET.get('q')
+        print(f"Query param q: {q}")
+        return JsonResponse({'valor': valor, 'query': q})
+
+    else:
+        return JsonResponse({"error": "HTTP method not supported"}, status=405)
+
+def prueba7(request):
+    if request.method == "GET":
+        q = request.GET.get('q')
+        p = request.GET.get('p')
+        print(f"q = {q}, p = {p}")
+        return JsonResponse({'q': q, 'p': p})
+
+    else:
+        return JsonResponse({"error": "HTTP method not supported"}, status=405)
