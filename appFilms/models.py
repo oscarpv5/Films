a7c058247e7f4f33549d7a7500873722ce79a32b
opilladov <opilladov24@fpcoruna.afundacion.org>
2025-04-22 12:03:55 +0200

    Implemented models.py

--- appFilms/models.py
+++ appFilms/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.db.models import SET_NULL
 
-# Create your models here.
+class Film(models.Model):
+    title = models.CharField(primary_key=True, max_length=50)
+
+class Actor(models.Model):
+    actorName = models.CharField(max_lenght=50)
+
+class ActorFilm(models.Model):
+    title = models.ForeignKey("Film", on_delete=SET_NULL, null=True)
+    actorName = models.ForeignKey("Actor", on_delete=SET_NULL, null=True)
+
+class User(models.Model):
+    userName = models.CharField(primary_key=True, max_lenght=50)
+
+class Score(models.Model):
+    filmScore = models.IntegerField()
+    film = models.ForeignKey("Film", on_delete=SET_NULL, null=True)
+    userName = models.ForeignKey("User", on_delete=SET_NULL, null=True)
