309338102ccffff3d975a2002849d07aee19c387
opilladov <opilladov24@fpcoruna.afundacion.org>
2025-04-23 12:48:00 +0200

    Example tests with another model from models.py

--- appFilms/models.py
+++ appFilms/models.py
@@ -18,3 +18,42 @@ class Score(models.Model):
     filmScore = models.IntegerField()
     film = models.ForeignKey("Film", on_delete=SET_NULL, null=True)
     userName = models.ForeignKey("User", on_delete=SET_NULL, null=True)
+
+'''
+class Seleccion(models.Model):
+    nombre = models.CharField(max_lenght=50)
+
+class Equipo(models.Model):
+    nombre = models.CharField(max_lenght=50)
+
+class Jugador(models.Model):
+    nombre = models.CharField(max_lenght=50)
+    posicion = models.CharField(max_lenght=50)
+    equipo = models.ForeignKey("Equipo", on_delete=SET_NULL, null=True)
+    seleccion = models.ForeignKey("Seleccion", on_delete=SET_NULL, null=True)
+
+class Competicion(models.Model):
+    categoria = models.CharField(max_lenght=50)
+    type = models.CharField(max_length=2, choices=[
+        ('EQ', 'Equipo'),
+        ('SE', 'Seleccion'),
+    ])
+
+class EquipoCompeticion(models.Model):
+    equipo = models.ForeignKey("Equipo", on_delete=SET_NULL, null=True)
+    competicion = models.ForeignKey("Competicion", on_delete=SET_NULL, null=True)
+
+class SeleccionCompeticion(models.Model):
+    seleccion = models.ForeignKey("Seleccion", on_delete=SET_NULL, null=True)
+    competicion = models.ForeignKey("Competicion", on_delete=SET_NULL, null=True)
+
+class EquipoPartido(models.Model):
+    equipo1 = models.ForeignKey("Equipo", on_delete=SET_NULL, null=True)
+    equipo2 = models.ForeignKey("Equipo", on_delete=SET_NULL, null=True)
+    competicion = models.ForeignKey("Competicion", on_delete=SET_NULL, null=True)
+
+class SeleccionPartido(models.Model):
+    seleccion1 = models.ForeignKey("Seleccion", on_delete=SET_NULL, null=True)
+    seleccion2 = models.ForeignKey("Seleccion", on_delete=SET_NULL, null=True)
+    competicion = models.ForeignKey("Competicion", on_delete=SET_NULL, null=True)
+'''
