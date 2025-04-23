ffdf003136d2ac03358f0f119476b824643400e7
opilladov <opilladov24@fpcoruna.afundacion.org>
2025-04-23 13:17:03 +0200

    Migration and admin site register

--- appFilms/admin.py
+++ appFilms/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Film, Actor, ActorFilm, User, Score
 
-# Register your models here.
+admin.site.register(Film)
+admin.site.register(Actor)
+admin.site.register(ActorFilm)
+admin.site.register(User)
+admin.site.register(Score)
